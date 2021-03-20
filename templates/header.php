0f4fe036ada250cb584f3be5e697daa3bbb7b92a
Veljko Mihajlovic <hailranger@yahoo.com>
2021-03-20 19:34:13 +0100

    Prva verzija

--- templates/header.php
+++ templates/header.php
@@ -20,22 +20,23 @@
 </head>
     
 <body> 
-<<<<<<< HEAD
-=======
- 
-<nav class="topnav navbar navbar-expand-lg navbar-dark bg-black fixed-top">
-<div class="container-fluid">
-	<a class="navbar-brand" href="/"><img style="width: 100px" src="./templates/assets/img/EC_logo.png" alt="EestecChallenge"></a>
-	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-	<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="navbar-collapse collapse" id="navbarColor02" style="">
-		<ul class="navbar-nav mr-auto d-flex align-items-center">
-			<li class="nav-item">
-			    <a class="nav-link" href="/">Početna</a>
-			</li>
-		</ul>
-	</div>
-</div>
-</nav>
->>>>>>> 2415d2dda916600cca978cda1f8999a5eb43f8db
+
+<header>
+    <div class="jumbotron jumbotron-lg jumbotron-fluid mb-0 pb-3 bg-primary position-relative" style="padding: 3rem 1rem;">
+        <div class="container-fluid text-white h-100">
+            <div class="d-lg-flex align-items-center justify-content-between text-center pl-lg-5">
+                <div class="col pt-4 pb-4">
+                    <h1 class="display-3">Cicada<strong>3301 TI</strong></h1>
+                    <h5 class="font-weight-light mb-4">Proverite izvor <strong>od potencijalnih napada <i class="fas fa-shield-alt fa-2x text-info"></i></strong></h5>
+                    <a href="#search" class="btn btn-lg btn-outline-white btn-round">Pretraži</a>
+                </div>
+                <div class="col align-self-bottom align-items-right text-right h-max-380 h-xl-560 position-relative z-index-1">
+                    <img src="http://www.pngmart.com/files/7/Cyber-Security-Transparent-PNG.png" class="rounded img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+    <svg style="-webkit-transform:rotate(-180deg); -moz-transform:rotate(-180deg); -o-transform:rotate(-180deg); transform:rotate(-180deg); margin-top: -1px;" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1440 126" xml:space="preserve">
+    <path class="bg-primary" d="M685.6,38.8C418.7-11.1,170.2,9.9,0,30v96h1440V30C1252.7,52.2,1010,99.4,685.6,38.8z"></path>
+    </svg>
+</header>
